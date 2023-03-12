0798c2e00a0e5eb5fac3206c7a84ef21e4e237b8
RobDawg-sama <pickles40324@gmail.com>
2023-03-12 12:21:38 -0400

    added a copy button to each clipboard entry allowing you to copy it to system keyboard

--- src/components/draggable.js
+++ src/components/draggable.js
@@ -9,18 +9,34 @@ const Board = () => {
     setPosition({ x: data.x, y: data.y });
   };
   const handleAddEntry = () => {
-    //use a python api to copy from clipboard
-    setEntries([...entries, newEntry]);
-    setNewEntry("");
+    if (newEntry) {
+      setEntries([...entries, newEntry]);
+      setNewEntry("");
+    }
   };
   const handleEntryChange = (event) => {
     setNewEntry(event.target.value);
   };
 
-  const clipBoardCopy = async () => {
-    const response = await axios.get('http://localhost:5000/clipboard');
+  const clipBoardPaste = async () => {
+    const response = await axios.get("http://localhost:5000/clipboard");
     setNewEntry(response.data);
-  }
+  };
+
+  const clipBoardCopy = async (index) => {
+    console.log(entries[index]);
+    const data = { text: entries[index] };
+    axios
+      .post("http://localhost:5000/clipboard", data, {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
   const handleDelete = (index) => {
     const newEntries = [...entries];
     newEntries.splice(index, 1);
@@ -40,12 +56,16 @@ const Board = () => {
               value={newEntry}
               onChange={handleEntryChange}
             />
-            <button onClick={clipBoardCopy}>Paste from clipboard</button>
+            <button onClick={clipBoardPaste}>Paste from clipboard</button>
             <button onClick={handleAddEntry}>Add Entry</button>
           </div>
           <ol>
             {entries.map((entry, index) => (
-              <li key={index}>{entry}<button onClick={() => handleDelete()}>Delete</button></li>
+              <li key={index}>
+                {entry}
+                <button onClick={() => handleDelete()}>Delete</button>
+                <button onClick={() => clipBoardCopy(index)}>Copy</button>
+              </li>
             ))}
           </ol>
         </div>
